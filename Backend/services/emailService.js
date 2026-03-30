909ce6951188cd68c798327bd79e3fe0fadec71a
HarishReddy1612 <harishreddy1612@gmail.com>
2026-03-30 20:09:27 +0530

    Added one line family 4 to ignore ipv6  and to  use ipv4

--- Backend/services/emailService.js
+++ Backend/services/emailService.js
@@ -23,6 +23,7 @@ const sendEmail = async (email, otp) => {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
+      family: 4
     });
 
     const mailOptions = {
